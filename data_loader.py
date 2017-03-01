3dab3976fb4fefc6ab03a72557448b49254b9a17
Konrad Wagstyl <konradwagstyl@wpa163035.wireless.mcgill.ca>
2017-03-01 16:28:48 -0500

    outline

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,30 @@
 import io_mesh as io
+
+##loading data
+#load limiting mask i.e. vertices to include
+
+#load mesh file
+io.load_mesh_geometry(
+
+#load subject overlays
+
+#load subject labels
+
+#mask out all vertices not of interest
+
+
+#create graph from neighbours
+
+#create feature matrix from features
+
+
+#create target labels from subject labels
+
+
+#train neural network
+
+
+
+#figure out validation
+
+
