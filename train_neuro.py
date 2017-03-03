ae09692dc85fb158f94da4371c883052b8090231
Konrad Wagstyl <konradwagstyl@Konrads-Macintosh.local>
2017-03-03 10:46:50 -0500

    latest debug

--- train_neuro.py
+++ train_neuro.py
@@ -34,7 +34,9 @@ adj = io.mmread('./data/all_subjects_adjacency.mtx')
 #
 label = np.loadtxt('./data/Label_data_in_mask.txt', dtype=int).flatten()
 features_mat = np.load('./data/feature_matrix.npy')
-features = np.reshape(features_mat, 
+features = np.reshape(features_mat,[len(label),np.shape(features_mat)[2]])
+features = sparse.csr_matrix(features)
+
 
 classes = np.array([label[:],1+label[:]*-1])
 train = 40
@@ -102,24 +104,19 @@ cost_val = []
 
 # Train model
 for epoch in range(FLAGS.epochs):
-
     t = time.time()
     # Construct feed dictionary
     feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders)
     feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-
     # Training step
     outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
-
     # Validation
     cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders)
     cost_val.append(cost)
-
     # Print results
     print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
           "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
           "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
-
     if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping+1):-1]):
         print("Early stopping...")
         break
